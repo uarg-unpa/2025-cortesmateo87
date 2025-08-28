99911c2007526327981c975ef4bd4f1e82121792
Mateo Mercado <mateomercado1306@gmail.com>
2025-08-28 21:23:36 +0000

    ..

--- tp2/Persona.java
+++ tp2/Persona.java
@@ -53,16 +53,4 @@ public class Persona {
             return false;
         }
     }
-
-    public class testPersona(){
-        public static void main(Strings[] args){
-            int num;
-            boolean esmayor;
-            Persona persona1, persona2;
-            persona1 = new.Persona("Franco","Herrena","42.122.661","21");
-            persona2 = new.Persona("Felipe","Costa","41.2ll.521","32");
-            persona1.MayorDeEdad();
-            esmayor = persona2.MayorDeEdad();
-        }
-    }
 }
