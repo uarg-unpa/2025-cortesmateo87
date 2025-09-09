09e319a6fc6cb4af8f605fe48599f520c8972111
Mateo Mercado <mateomercado1306@gmail.com>
2025-09-09 17:47:02 +0000

    /

--- practica clase C&O/testlibro.java
+++ practica clase C&O/testlibro.java
@@ -1,5 +1,7 @@
-public static void main(String[] args){
-    Libro libro1 = new Libro ("Harry Potter","Ch. R","1997","368")
-    Libro libro2 = new Libro ("Harry Potter","Ch. R","1997","368")
-    Libro libro3 = new Libro ("Harry Potter","Ch. R","1997","368")
+public class Personaje{
+    public static void main(String[] args){
+        Libro libro1 = new Libro ("Harry Potter","Ch. R","1997","368");
+        Libro libro2 = new Libro ("Harry Potter","Ch. R","1997","368");
+        Libro libro3 = new Libro ("Harry Potter","Ch. R","1997","368");
+    }
 }
