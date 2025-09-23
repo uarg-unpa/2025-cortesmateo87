9763e5b07d53a9343200810d3d62ac68f56bc622
Mateo Mercado <mateomercado1306@gmail.com>
2025-09-23 01:41:55 -0300

    .

---
+++ tp2Parte2/ejer2/Estacion.java
@@ -0,0 +1,29 @@
+package tp2Parte2.ejer2;
+
+public class Estacion {
+    private double capacidadMAX;
+    private double capacidadActual;
+
+    public Estacion(double capacidadActual, double capacidadMAX){
+        this.capacidadActual = capacidadActual;
+        this.capacidadMAX = capacidadMAX;
+    }
+
+    //get y set
+    public double getCapacidadActual(){
+        return capacidadActual;
+    }
+    public void setCapacidadActual(double capacidadActual){
+        this.capacidadActual = capacidadActual;
+    }
+
+    public double getCapacidadMAX(){
+        return capacidadMAX;
+    }
+    public void setCapacidadMAX(double capacidadMAX){
+        this.capacidadMAX = capacidadMAX;
+    }
+
+    // METODOS
+    
+}
